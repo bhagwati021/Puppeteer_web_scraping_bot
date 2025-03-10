e7cb3c2c7c7166dcc26c88af626e4574e162d5a4
bhagwati021 <bhagwatibashyal1234@gmail.com>
2025-03-10 17:00:25 +0530

    delete

--- models/MetadataModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-// Stores metadata like lastUsedIndex
-const metadataSchema = new mongoose.Schema({
-  key: { type: String, unique: true, required: true }, 
-  value: { type: Number, default: -1 }
-});
-
-export const Metadata = mongoose.model("Metadata", metadataSchema);
